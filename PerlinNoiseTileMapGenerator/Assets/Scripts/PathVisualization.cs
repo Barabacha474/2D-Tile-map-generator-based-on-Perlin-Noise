12dbd88a312589b3b790dfab54dce6ebdf6c739a
NikitaRUNI <cherkashin-nikita22@mail.ru>
2024-12-05 20:25:34 +0300

    Minor visualization fixes

--- PerlinNoiseTileMapGenerator/Assets/Scripts/PathVisualization.cs
+++ PerlinNoiseTileMapGenerator/Assets/Scripts/PathVisualization.cs
@@ -3,31 +3,31 @@ using UnityEngine;
 
 public class PathVisualization : MonoBehaviour
 {
-    public Vector2Int PixelPosition; // The position of the red pixel (input)
+    public Vector2Int PixelPosition;
 
-    public int GridWidth = 150; // Width of the grid
-    public int GridHeight = 150; // Height of the grid
-    public GameObject PixelPrefab; // Prefab for a pixel (a small square or cube)
+    private int GridWidth;
+    private int GridHeight;
+    public Color color = Color.red;
+    public GameObject PixelPrefab;
 
     void Start()
     {
-        // Draw the pixel at the specified position
+        GridWidth = FindAnyObjectByType<PerlinNoiseTileMapGeneration>().GridWidth;
+        GridHeight = FindAnyObjectByType<PerlinNoiseTileMapGeneration>().GridHeight;
+
         DrawPixel(PixelPosition);
     }
 
     public void DrawPixel(Vector2Int position)
     {
-        // Ensure the pixel position is within grid bounds
         if (position.x >= 0 && position.x < GridWidth && position.y >= 0 && position.y < GridHeight)
         {
-            // Instantiate a red pixel at the specified position
-            GameObject pixel = Instantiate(PixelPrefab, new Vector3(position.x - GridWidth / 2, position.y - GridHeight / 2, -1), Quaternion.identity);
+            GameObject pixel = Instantiate(PixelPrefab, new Vector3(position.x - GridWidth / 2, position.y - GridHeight / 2, -1), Quaternion.identity, transform);
             
-            // Set the pixel color to red
             var renderer = pixel.GetComponent<SpriteRenderer>();
             if (renderer != null)
             {
-                renderer.color = Color.red;
+                renderer.color = color;
             }
         }
         else
